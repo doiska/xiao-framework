bf39751ec20ce6333de4f0b914c6fdb902cf149a
doiská <doiskka@gmail.com>
2022-08-29 09:04:19 -0300

    Renamed event to LocalEvent

--- src/decorators/event.decorator.ts
+++ src/decorators/event.decorator.ts
@@ -1,7 +1,7 @@
 import { extendArrayMetadata } from "@utils/extend-metadata.utils";
 import { EVENT_METADATA } from "@decorators/constants";
 
-export function Event(eventName: string): MethodDecorator {
+export function LocalEvent(eventName: string): MethodDecorator {
 	return (_target: any, _propertyKey?: string | symbol, descriptor?: TypedPropertyDescriptor<any>) => {
 		extendArrayMetadata(EVENT_METADATA, [eventName], descriptor.value);
 		return descriptor;
